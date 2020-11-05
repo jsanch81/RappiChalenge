b92580f8f192ba37120882b5c6b43e6f70587c5c
admin <admin@PTOF70255.cinfo.eafit.edu.co>
2020-11-05 15:52:09 -0500

    GNRAL: se añaden las actualizaciones en el docker compose

---
+++ apiProject/app/modelApi.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from flask import Flask
+from flask import request
+from controller.ClasificationModel import ClasificationModel
+app = Flask(__name__)
+
+@app.route('/api/get/prediction', methods=['GET'])
+def predict():
+    data = request.json
+    model = ClasificationModel(data)
+    result = model.predict()
+    return result.to_dict('index')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- apiProject/execute.sh
+++ apiProject/execute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export FLASK_APP=./api/modelApi.py
+export FLASK_APP=./app/modelApi.py
 flask run
